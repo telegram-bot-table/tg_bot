fdc3c8ba516ab12551ad8d62afaae6fef10f7500
ValyMihalchuk <valy378@gmail.com>
2021-03-29 22:19:12 +0300

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,34 +4,26 @@ import datetime
 from datetime import date
 
 
-date_entry = input('Введите данные в формате YYYY-MM-DD ')
-year, month, day = map(int, date_entry.split('-'))
-p = date(year, month, day).weekday()
-print(p)
-url ='https://ruz.spbstu.ru/faculty/99/groups/30687/?date='+date_entry
-#'+str(year)+'-'+str(month)+'-'+str(day-p)
-print(url)
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-items = soup.find_all('li', class_='schedule__day')
+#in: date(YYYY-MM-DD), dir id, group id
+#out: list with shedule
+def get_data(date_entry, id_dir=99, id_group=30687):
+    year, month, day = map(int, date_entry.split('-'))
+    p = date(year, month, day).weekday()
+    url ='https://ruz.spbstu.ru/faculty/'+str(id_dir)+'/groups/'+str(id_group)+'/?date='+date_entry
 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    items = soup.find_all('li', class_='schedule__day')
 
-
-i = -1
-for n in items:
-    i = i+1
-    if i is not p:
-        continue
-    print(i)
-    
-    items1 = n.find_all('li', class_='lesson')
+    list = []
+    items1 = items[p].find_all('li', class_='lesson')
     for k in items1:
         TimeAndLesson1 = k.find('div', class_='lesson__subject').text.strip()
-        print(f'{TimeAndLesson1 }  ')
-     
-    #TimeAndLesson1 = i.find('div', class_='lesson__subject').text.strip()
-    #TimeAndLesson2 = i.find('div', class_='lesson__subject').text.strip()
-    #TimeAndLesson3 = i.find('div', class_='lesson__subject').text.strip()
+        Teacher = k.find('div', class_='lesson__teachers').text.strip()
+        Place = k.find('div', class_='lesson__places').text.strip()
+        d =(TimeAndLesson1,Teacher, Place)
+        list.append(d)
+    return list
+       
 
-
-    #print(f'{n}:  {TimeAndLesson1} и еще {TimeAndLesson2}  ')
+print(get_data(input(' YYYY-MM-DD ')))
